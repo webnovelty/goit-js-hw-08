08ab0185dc96c896599a1eb6c59387dac98d471b
webnovelty <webnoveltystudio@gmail.com>
2022-08-04 23:12:10 +0200

    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+galleryEl.insertAdjacentHTML('beforeend', createGalleryItems(galleryItems));
+function createGalleryItems(galleryItems) {
+	return galleryItems.map((galleryItems) => {
+		return `
+		<a class="gallery__item" href = "${galleryItems.original}" >
+				<img class="gallery__image" src="${galleryItems.preview}" alt="${galleryItems.description}" />
+</a>`
+			;
+	}).join('');
+
+}
+
+
+let gallery = new SimpleLightbox('.gallery a');
+gallery.on('show.simplelightbox', function () {
+	gallery.options.captionsData = 'alt';
+	gallery.options.captionDelay = 250;
+});
