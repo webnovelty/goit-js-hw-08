c472db036aacf6af650e4f8582c7917f38386878
webnovelty <webnoveltystudio@gmail.com>
2022-08-05 21:08:02 +0200

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+const dataSave = function (data) {
+	localStorage.setItem("videoplayer-current-time", data.seconds);
+}
+player.on('timeupdate', throttle(dataSave, 1000));
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then(function () {
+}).catch(function (error) {
+	switch (error.name) {
+		case 'RangeError':
+			break;
+		default:
+			break;
+	}
+});
+
 
